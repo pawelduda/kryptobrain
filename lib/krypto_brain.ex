984a840f0d1d387495d1d41b43a0401d7a40329e
pawelduda <pawelduda91@gmail.com>
2017-04-19 20:57:38 +0200

    Init mix app

---
+++ lib/krypto_brain.ex
@@ -0,0 +1,18 @@
+defmodule KryptoBrain do
+  @moduledoc """
+  Documentation for KryptoBrain.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KryptoBrain.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/krypto_brain_test.exs
@@ -0,0 +1,8 @@
+defmodule KryptoBrainTest do
+  use ExUnit.Case
+  doctest KryptoBrain
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
