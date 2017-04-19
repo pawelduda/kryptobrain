a115faac842cf06dcd9474c7f891afb81d13c4f1
pawelduda <pawelduda91@gmail.com>
2017-04-19 23:07:48 +0200

    Handle received ticker update

--- lib/krypto_brain/api/poloniex.ex
+++ lib/krypto_brain/api/poloniex.ex
@@ -2,13 +2,30 @@ defmodule KryptoBrain.Api.Poloniex do
   @wsuri "wss://api.poloniex.com"
   @topic "ticker"
 
-  def connect do
+  def subscribe_to_ticker do
+    # {:ok, _} = Crossbar.start()
+    {:ok, connection} = Spell.connect(@wsuri, realm: "realm1", roles: [Spell.Role.Subscriber])
+    {:ok, subscription} = Spell.call_subscribe(connection, @topic)
+
+    receive_event(connection, subscription)
+  end
+
+  defp receive_event(connection, subscription) do
+    case Spell.receive_event(connection, subscription) do
+      {:ok, event} -> save_event(event)
+      {:error, reason} -> {:error, reason}
+    end
+
+    receive_event(connection, subscription)
+  end
+
+  defp save_event(%{arguments: data}) do
     # Appropriate labels for these data are, in order:
     # currencyPair, last, lowestAsk, highestBid, percentChange, baseVolume,
     # quoteVolume, isFrozen, 24hrHigh, 24hrLow
-
-    # {:ok, _} = Crossbar.start()
-    {:ok, subscriber} = Spell.connect(@wsuri, realm: "realm1", roles: [Spell.Role.Subscriber])
-    {:ok, subscription} = Spell.call_subscribe(subscriber, @topic)
+    [
+      currency_pair, last, lowest_ask, highest_bid, percent_change,
+      base_volume, quote_volume, is_frozen, l24_hr_high, l24_hr_low
+    ] = data
   end
 end
