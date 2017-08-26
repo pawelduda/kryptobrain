188eb3bee969b288d7aa81fb940581a82e3bebda
pawelduda <pawelduda91@gmail.com>
2017-08-26 15:20:45 +0200

    Refactor

--- lib/krypto_brain/trading/output_table_printer_bittrex.ex
+++ lib/krypto_brain/trading/output_table_printer_bittrex.ex
@@ -65,25 +65,19 @@ defmodule KryptoBrain.Trading.OutputTablePrinterBittrex do
 
     IO.puts("\n")
     TableRex.Table.new(rows, @headers)
-    |> TableRex.Table.put_column_meta(1, color: fn(text, value) ->
-      case value do
-        "⬆️ BUY" -> [:green, text]
-        "HOLD" -> [:blue, text]
-        "⬇️ SELL" -> [:red, text]
-        _ -> text
-      end
-    end)
+    |> TableRex.Table.put_column_meta(1, color: &add_color_meta_to_signal_column/2)
     |> TableRex.Table.render!
     |> IO.puts
   end
 
-  defp signal_int_to_str(signal_int) do
-    case signal_int do
-      C._BUY -> "⬆️ BUY"
-      C._HOLD -> "HOLD"
-      C._SELL -> "⬇️ SELL"
-    end
-  end
+  defp signal_int_to_str(C._BUY), do: "⬆️ BUY"
+  defp signal_int_to_str(C._HOLD), do: "HOLD"
+  defp signal_int_to_str(C._SELL), do: "⬇️ SELL"
+
+  defp add_color_meta_to_signal_column(text, "⬆️ BUY"), do: [:green, text]
+  defp add_color_meta_to_signal_column(text, "HOLD"), do: [:blue, text]
+  defp add_color_meta_to_signal_column(text, "⬇️ SELL"), do: [:red, text]
+  defp add_color_meta_to_signal_column(text, _value), do: text
 
   defp format_price_as_satoshis(float_price) do
     :erlang.float_to_binary(float_price, [decimals: 8])
